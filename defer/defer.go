09e6f522e7d3d2e5ff8d1e3845ab1bde69e0c553
Ninux <ninux.ad@gmail.com>
2018-05-13 23:26:21 -0700

    added comments

--- defer/defer.go
+++ defer/defer.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func do() {
-	defer fmt.Println("First")
-	defer fmt.Println("Second")
-	defer fmt.Println("Third")
+
+	n := 1 //Even if you are using defer the variables are calculated first than any statements
+
+	// for the below statements, defer will pass on the control to the next statement to execute first.
+	defer fmt.Println("First", n)
+	n = 2
+	defer fmt.Println("Second", n)
+	n = 3
+	defer fmt.Println("Third", n)
 }
 
 func main() {
